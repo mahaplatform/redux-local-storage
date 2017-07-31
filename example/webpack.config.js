aadc88a33cc722190275eb5d614066fb7368ee27
Greg Kops <greg@thinktopography.com>
2017-07-31 14:52:03 -0400

    updating example webpack config

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -26,13 +26,8 @@ module.exports = (env) => ({
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            'babel-preset-es2015',
-            'babel-preset-react',
-            'babel-preset-stage-0'
-          ].map(require.resolve)
+        use: {
+          loader: 'babel-loader?+babelrc,+cacheDirectory'
         }
       }
     ]
